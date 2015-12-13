1ad995a2b9f3fc3444494886d0abaefd72e91603
Vineet Kumar <vineet7kumar@gmail.com>
2015-12-13 16:41:31 -0500

    added documentation

--- homepage.sh
+++ homepage.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Switch to my homepage's pelican directory and invoke virtualenv.
+#Run like . homepage.sh or source homepage.sh
+
 PELICAN=/Volumes/data/work/virtualenvs/pelican
 cd $PELICAN/home
 source $PELICAN/bin/activate
