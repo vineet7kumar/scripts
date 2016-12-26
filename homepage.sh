db20727b07b16a1a551bd43094812271ce32e8fc
Vineet Kumar <vineet.kumar@mail.mcgill.ca>
2016-12-26 13:40:22 -0500

    Deleting homepage.sh. No use anymore

--- homepage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Switch to my homepage's pelican directory and invoke virtualenv.
-#Run like . homepage.sh or source homepage.sh
-
-PELICAN=/Volumes/data/work/virtualenvs/pelican
-cd $PELICAN/home
-source $PELICAN/bin/activate
