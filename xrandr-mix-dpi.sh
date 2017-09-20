e644959398420a4f6f66e2a93d4730ff15737a51
vineet <vineet7kumar@gmail.com>
2017-09-20 16:33:53 -0400

    updated xrandr fix script to first restore to default state

--- xrandr-mix-dpi.sh
+++ xrandr-mix-dpi.sh
@@ -3,4 +3,5 @@
 # screen for the high DPI monitor.
 # See more about xrandr and high DPI settings on arch wiki.
 
+xrandr --output DP-0 --auto --primary --scale 1x1 --output HDMI-0 --auto --scale 1x1 --right-of DP-0
 xrandr --output DP-0 --auto --primary --output HDMI-0 --auto --scale 1.5x1.5 --right-of DP-0
