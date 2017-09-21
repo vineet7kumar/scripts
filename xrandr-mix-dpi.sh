f671d5a5f3586aef52dae0eee31aa84fad6333a3
vineet <vineet7kumar@gmail.com>
2017-09-21 14:43:21 -0400

    fixed panning issue

--- xrandr-mix-dpi.sh
+++ xrandr-mix-dpi.sh
@@ -3,5 +3,6 @@
 # screen for the high DPI monitor.
 # See more about xrandr and high DPI settings on arch wiki.
 
-xrandr --output DP-0 --auto --primary --scale 1x1 --output HDMI-0 --auto --scale 1x1 --right-of DP-0
-xrandr --output DP-0 --auto --primary --output HDMI-0 --auto --scale 1.5x1.5 --right-of DP-0
+#xrandr --output DP-0 --auto --primary --panning 3840x2160 --output HDMI-0 --auto --scale 1.5x1.5 --right-of DP-0
+xrandr --output DP-0 --auto --primary --panning 3840x2160 --output HDMI-0 \
+        --auto --scale 1.5x1.5 --pos 3840x540
